45762c15cf319d46cff8f46938353a53ef2ed4a5
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2021-07-08 09:29:52 -0200

    add converter routines for waveforms

---
+++ src/Converter.cc
@@ -0,0 +1,108 @@
+#include <sys/types.h> 
+#include <dirent.h> 
+#include <sys/stat.h> 
+#include <unistd.h> 
+#include <algorithm> 
+#include <vector>
+#include "TTree.h" 
+#include "TFile.h" 
+#include "mattak/Waveforms.h"
+#include "mattak/Converter.h"
+#include "mattak/Header.h"
+#include "mattak/DAQStatus.h"
+#include "rno-g.h" 
+
+template <typename T> const char * getName() { return ""; } 
+template<> const char * getName<mattak::Waveforms>() { return "wf"; } 
+template<> const char * getName<mattak::Header>() { return "hdr"; } 
+template<> const char * getName<mattak::DAQStatus>() { return "ds"; } 
+
+
+
+template <typename Traw, int(*ReaderFn)(rno_g_file_handle_t, Traw*), typename Troot>
+static int convert_impl(int N, const char ** infiles, const char * outfile, const char * treename, int station)
+{
+  TFile f(outfile,"RECREATE"); 
+  if (!treename) treename = getName<Troot>(); 
+  TTree * t = new TTree(treename,treename); 
+  Troot * b = new Troot; 
+  t->Branch(treename, &b); 
+
+  int nprocessed = 0; 
+  for (int i = 0; i < N; i++) 
+  {
+
+    rno_g_file_handle h; 
+    rno_g_init_handle(&h, infiles[i], "r"); 
+
+    Traw raw; 
+    while (ReaderFn(h, &raw) > 0) 
+    {
+      nprocessed++; 
+      b = new (b) Troot(&raw); 
+      if (station > 0) b->station_number = station; 
+      t->Fill(); 
+    }
+    rno_g_close_handle(&h); 
+  }
+
+  f.Write(); 
+
+  return nprocessed; 
+}
+
+
+
+
+template <typename Traw, int (*ReaderFn)(rno_g_file_handle_t, Traw*), typename Troot>
+static int convert_dir(const char * dir, const char * outfile, const char * treename, int station)
+{
+  std::vector<std::string> files; 
+  std::vector<const char *> file_ptrs; 
+  DIR * dirp = opendir(dir); 
+  if (!dirp) 
+  {
+    fprintf(stderr,"Could not open dir %s\n", dir); 
+    return 0; 
+  }
+
+  struct dirent * dent; 
+
+
+  while ((dent = readdir(dirp)))
+  {
+    std::string fname = dir; 
+    fname+="/"; 
+    fname+=dent->d_name; 
+    files.push_back(fname); 
+  }
+
+  closedir(dirp); 
+  std::sort(files.begin(), files.end()); 
+
+  file_ptrs.reserve(files.size()); 
+  for (auto f : files) 
+  {
+    file_ptrs.push_back(f.c_str()); 
+  }
+
+  return convert_impl<Traw,ReaderFn,Troot>(file_ptrs.size(), &file_ptrs[0], outfile, treename,station); 
+}
+
+
+int mattak::convert::convertWaveformFiles(int nfiles, const char ** infiles, const char * outfile, const char * treename,int station) 
+{
+  return convert_impl<rno_g_waveform_t, rno_g_waveform_read, mattak::Waveforms>(nfiles, infiles, outfile, treename,station); 
+}
+
+int mattak::convert::convertWaveformFile(const char * infile, const char * outfile, const char * treename,int station)
+{
+  return convert_impl<rno_g_waveform_t, rno_g_waveform_read, mattak::Waveforms>(1, &infile, outfile, treename,station); 
+}
+
+int mattak::convert::convertWaveformDir(const char * dir, const char * outfile, const char * treename,int station) 
+{
+  return convert_dir<rno_g_waveform_t, rno_g_waveform_read, mattak::Waveforms>(dir, outfile, treename,station); 
+}
+
+
