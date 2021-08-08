2c04330414f05e9255531d206977fda7bb8eb2bc
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2021-08-08 16:10:16 -0500

    save station number

--- src/Waveforms.cc
+++ src/Waveforms.cc
@@ -17,6 +17,7 @@ mattak::Waveforms::Waveforms(const rno_g_waveform_t * wf )
   this->run_number = wf->run_number; 
   this->event_number = wf->event_number; 
   this->buffer_length = wf->radiant_nsamples; 
+  this->station_number = wf->station; 
   for (unsigned i = 0; i < mattak::k::num_radiant_channels; i++) 
   {
     memcpy(this->radiant_data[i], wf->radiant_waveforms[i], sizeof(int16_t) * mattak::k::num_radiant_samples); 
