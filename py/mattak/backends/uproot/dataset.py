62724f5e1e0181906bc4bcbd269142e0991aca0a
Felix Schlüter <fschlueter@icecube.wisc.edu>
2023-03-01 15:18:26 +0100

    Refactor dataset._iterate from uproot backend for better readablity

--- py/mattak/backends/uproot/dataset.py
+++ py/mattak/backends/uproot/dataset.py
@@ -215,33 +215,28 @@ class Dataset(mattak.Dataset.AbstractDataset):
     def _iterate(self, start, stop, calibrated, max_in_mem,
                  selector: Optional[Callable[[mattak.Dataset.EventInfo], bool]] = None) -> Tuple[mattak.Dataset.EventInfo, numpy.ndarray]:
 
-        current_start = -1 
-        current_stop = -1
-        w = None
-        e = None
-        i = start 
-        preserve_entries = self.entry
-        if self.multiple:
-            preserve_entries = (self.first, self.last) 
-        while i < stop: 
-            if i >= current_stop:
-                current_start = i
-                current_stop = min(i+max_in_mem, stop)
-                self.setEntries((current_start, current_stop)) 
-                w = self.wfs(calibrated)
-                e = self.eventInfo()
-                self.setEntries(preserve_entries) # hide that we're modifying these 
+        # determine in how many batches we want to access the data given how much events we want to load into the RAM at once
+        n_batches = (stop - start) // max_in_mem + 1
 
-            rel_i = i - current_start
-            i += 1
-            if selector is not None:
-                if selector(e[rel_i]):
-                    yield e[rel_i], w[rel_i]
-            else:
-                yield e[rel_i], w[rel_i]
+        for i_batch in range(n_batches):
 
- 
-
+            # looping over the batches defining the start and stop index
+            batch_start = start + i_batch * max_in_mem
+            batch_stop = min(stop, batch_start + max_in_mem)
+            
+            self.setEntries((batch_start, batch_stop))
+            
+            # load events from file 
+            w = self.wfs(calibrated)
+            e = self.eventInfo()
+            
+            for idx in range(batch_stop - batch_start):
+                if selector is not None:
+                    if selector(e[idx]):
+                        yield e[idx], w[idx]
+                else:
+                    yield e[idx], w[idx]
+            
 
 
 
