83ffc8bf1d57048ee6e36a6e62749ddd8094bdff
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-02-14 11:43:54 +0100

    Fix indentation. add voltage calibration to interface

--- py/mattak/Dataset.py
+++ py/mattak/Dataset.py
@@ -111,71 +111,71 @@ class AbstractDataset(ABC):
 def Dataset(station : int, run : int, data_dir : Optional[str] = None, backend : str= "auto",
             verbose : bool = False, skip_incomplete : bool = True,
             read_daq_status : bool = True, read_run_info : bool = True,
-            preferred_file : Optional[str] = None ) -> Optional[AbstractDataset]:
-   """
+            preferred_file : Optional[str] = None, voltage_calibration : Optional = None) -> Optional[AbstractDataset]:
+    """
 
-   This is not a class, but a factory method! This is meant to be the interface
-   to rule them all for loading RNO-G data. Due to Cosmin's poor initial API
-   design, it has become perhaps more complicated than it should be.
+    This is not a class, but a factory method! This is meant to be the interface
+    to rule them all for loading RNO-G data. Due to Cosmin's poor initial API
+    design, it has become perhaps more complicated than it should be.
 
-   If data_dir is a directory and station/run are non-zero, this returns a
-   dataset corresponding to the station and run using data_dir as the base
-   directory (i.e. the folder hierarcy is structured something likelike
-   ${data_dir}/stationX/runY/*.root). If data_dir is None, then the
-   environmental variable RNO_G_DATA (or RNO_G_ROOT_DATA) will be queried and
-   the run loaded from that base. data_dir can also be a URL for loading of
-   files via HTTP (e.g. https://user:password@example.com/rno-g-data), though
-   there may be some subtleties about escaping passwords that may differ
-   betweeen different backends.
+    If data_dir is a directory and station/run are non-zero, this returns a
+    dataset corresponding to the station and run using data_dir as the base
+    directory (i.e. the folder hierarcy is structured something likelike
+    ${data_dir}/stationX/runY/*.root). If data_dir is None, then the
+    environmental variable RNO_G_DATA (or RNO_G_ROOT_DATA) will be queried and
+    the run loaded from that base. data_dir can also be a URL for loading of
+    files via HTTP (e.g. https://user:password@example.com/rno-g-data), though
+    there may be some subtleties about escaping passwords that may differ
+    betweeen different backends.
 
-   In the special case of setting station = 0 and run = 0, data_dir
-   will be interpreted as a directory containing ROOT files, which is useful if
-   you don't have the full directory hierarchy setup or want to look at data
-   taken with the fakedaq.
+    In the special case of setting station = 0 and run = 0, data_dir
+    will be interpreted as a directory containing ROOT files, which is useful if
+    you don't have the full directory hierarchy setup or want to look at data
+    taken with the fakedaq.
 
-   If data_dir is, despite its name, a file rather than directory, then that
-   file will be attempted to be loaded as a combined file. Really it should
-   be called data_source, changing the parameter name will break the API and
-   we'll try hard not to do that.
+    If data_dir is, despite its name, a file rather than directory, then that
+    file will be attempted to be loaded as a combined file. Really it should
+    be called data_source, changing the parameter name will break the API and
+    we'll try hard not to do that.
 
-   The backend can be chosen explicitly ("pyroot" or "uproot") or auto will try
-   to use the best one ("pyroot" if available, otherwise reverting to "uproot").
+    The backend can be chosen explicitly ("pyroot" or "uproot") or auto will try
+    to use the best one ("pyroot" if available, otherwise reverting to "uproot").
 
-   verbose prints out things mostly useful for debugging.
+    verbose prints out things mostly useful for debugging.
 
-   read_daq_status and read_run_info are mostly self-explanatory. Avoiding
-   reading them may speed things up or work around the files not being there
-   for some reason.
+    read_daq_status and read_run_info are mostly self-explanatory. Avoiding
+    reading them may speed things up or work around the files not being there
+    for some reason.
 
-   skip_incomplete affects what happens when a dataset is incomplete
-   (telemetered). If True, will only index fully telemetered events. If False,
-   will be indexed by full dataset, but untelemetered events will have
-   waveforms of None type (if requested singly) or be all 0's (if requested
-   via the bulk interface, as numpy doesn't support jagged ararys).
+    skip_incomplete affects what happens when a dataset is incomplete
+    (telemetered). If True, will only index fully telemetered events. If False,
+    will be indexed by full dataset, but untelemetered events will have
+    waveforms of None type (if requested singly) or be all 0's (if requested
+    via the bulk interface, as numpy doesn't support jagged ararys).
 
-   preferred_file, if not None or "", and data_dir is not a file, will further
-   change the loading behavior. By default,  we will try to load full waveforms
-   falling back to loading combined.root. But if preferred_file is set, it will
-   prefer loading ${preferred_file}.root if possible, treating it as a file in
-   the same format as combined.root.  For example, you can set it to "combined"
-   to load combined.root even if full waveforms are available. Or, if you have
-   your own subselection in the same format (e.g. for example if you generated
-   a file that is only forced triggers) this provides an arguably convenient
-   way to load those.
+    preferred_file, if not None or "", and data_dir is not a file, will further
+    change the loading behavior. By default,  we will try to load full waveforms
+    falling back to loading combined.root. But if preferred_file is set, it will
+    prefer loading ${preferred_file}.root if possible, treating it as a file in
+    the same format as combined.root.  For example, you can set it to "combined"
+    to load combined.root even if full waveforms are available. Or, if you have
+    your own subselection in the same format (e.g. for example if you generated
+    a file that is only forced triggers) this provides an arguably convenient
+    way to load those.
 
 
-   """
+    """
 
-   if data_dir is None:
-       for env_var in ['RNO_G_DATA', 'RNO_G_ROOT_DATA']:
-           if env_var in os.environ:
-               data_dir = os.environ[env_var]
-               break
-       if data_dir is None:
-           print("Neither data_dir nor any relevant environmental variable (e.g. RNO_G_DATA) is defined and I don't know where else to look :(")
-           return None
+    if data_dir is None:
+        for env_var in ['RNO_G_DATA', 'RNO_G_ROOT_DATA']:
+            if env_var in os.environ:
+                data_dir = os.environ[env_var]
+                break
+        if data_dir is None:
+            print("Neither data_dir nor any relevant environmental variable (e.g. RNO_G_DATA) is defined and I don't know where else to look :(")
+            return None
 
-   if backend == "auto":
+    if backend == "auto":
         try:
             import ROOT
             import mattak.backends.pyroot.mattakloader
@@ -192,14 +192,16 @@ def Dataset(station : int, run : int, data_dir : Optional[str] = None, backend :
                 print("No backends available")
                 return None
 
-   if backend == "uproot":
+    if backend == "uproot":
         import mattak.backends.uproot.dataset
         return mattak.backends.uproot.dataset.Dataset(
             station, run, data_dir, verbose, skip_incomplete, read_daq_status, read_run_info, preferred_file)
-   elif backend == "pyroot":
+    elif backend == "pyroot":
         import mattak.backends.pyroot.dataset
         return mattak.backends.pyroot.dataset.Dataset(
-            station, run, data_dir, verbose, skip_incomplete, read_daq_status, read_run_info, preferred_file)
-   else:
-       print("Unknown backend (known backends are \"uproot\" and \"pyroot\")")
-       return None
+            station, run, data_dir, verbose=verbose, skip_incomplete=skip_incomplete, read_daq_status=read_daq_status,
+            read_run_info=read_run_info, preferred_file=preferred_file, voltage_calibration=voltage_calibration)
+
+    else:
+        print("Unknown backend (known backends are \"uproot\" and \"pyroot\")")
+        return None
