0f7581b3479638c203245a8d2461f59b44570727
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-02-16 11:48:21 +0100

    Syntax

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,23 +2,23 @@ import mattak.Dataset
 import time
 import numpy
 
-for backend in ("pyroot","uproot"):
-    print(backend) 
-    d = mattak.Dataset.Dataset(21,300,None,backend)
+
+for backend in ("pyroot", "uproot"):
+    print(backend)
+    d = mattak.Dataset.Dataset(21, 300, None, backend)
     print(d.N())
     print(d.eventInfo())
-    print(d.wfs()) 
+    print(d.wfs())
 
     d.setEntries((1,2))
     print(d.eventInfo())
-    print(d.wfs()) 
+    print(d.wfs())
 
     mean = 0
-    start = time.time() 
-    for ev in d.iterate(): 
-    #    print ( ev[0].eventNumber,numpy.average(ev[1]))
+    start = time.time()
+    for ev in d.iterate():
         mean += numpy.average(ev[1])
-    end = time.time() 
-    print (mean/d.N()) 
-    print ("time:", end-start) 
+    end = time.time()
 
+    print(mean / d.N())
+    print("time:", end - start)
