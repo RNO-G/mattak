ddefa0cb6acdb4ed87d58884a2a094cf3f1752d2
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2024-03-01 14:45:02 -0600

    add maximum, option for output

--- prog/rno-g-test-apply-voltage-calibration.cc
+++ prog/rno-g-test-apply-voltage-calibration.cc
@@ -10,7 +10,7 @@ int main (int nargs, char ** args)
 {
   if (nargs < 4) 
   {
-    fprintf(stderr,"usage: rno-g-test-apply-voltage-calibration station run vcfile\n"); 
+    fprintf(stderr,"usage: rno-g-test-apply-voltage-calibration station run vcfile max=10 output=1\n"); 
     return 1; 
   }
 
@@ -21,25 +21,35 @@ int main (int nargs, char ** args)
 
   int station = atoi(args[1]); 
   int run = atoi(args[2]); 
+  int max = nargs >= 4 ? atoi(args[4]) : 10; 
+  int output = nargs >=5 ? atoi(args[5]) : 1; 
 
   mattak::Dataset d(station,run, opt); 
 
+  if (max <=0 || max > d.N()) max = d.N(); 
   TCanvas c("c","c",1920,1080); 
-  for (int i = 0; i <  (d.N() < 10 ? d.N() : 10) ; i++) 
+  for (int i = 0; i <  max ; i++) 
   {
     d.setEntry(i); 
     printf("%d\n", i); 
     //do it twice, just in case
     for (int j = 0; j < 2; j++) 
     {
-      d.calibrated(j > 0)->drawWaveforms(mattak::WaveformPlotOptions(), &c); 
-      c.SaveAs(Form("vc_%d_%d.gif+50", station, run)); 
-      mattak::WaveformPlotOptions optraw; 
-      optraw.color = kRed +2; 
-      d.raw(j > 0)->drawWaveforms(optraw, &c); 
-      c.SaveAs(Form("vc_%d_%d.gif+50", station, run)); 
+      auto cal = d.calibrated(j > 0); 
+      auto raw = d.raw(j > 0); 
+
+      if (output) 
+      {
+        cal->drawWaveforms(mattak::WaveformPlotOptions(), &c); 
+        c.SaveAs(Form("vc_%d_%d.gif+50", station, run)); 
+        mattak::WaveformPlotOptions optraw; 
+        optraw.color = kRed +2; 
+        raw->drawWaveforms(optraw, &c); 
+        c.SaveAs(Form("vc_%d_%d.gif+50", station, run)); 
+      }
     }
   }
 
+  return 0; 
 
 }
