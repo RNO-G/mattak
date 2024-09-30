1ca4a970587924abeb8eabf33d7d8e2c31e1a2bb
Ruben Camphyn <rubencamphyn@gmail.com>
2024-09-30 21:05:14 +0200

    remove brackets in if or statement

--- scripts/convert_bias_scan_to_calibration.py
+++ scripts/convert_bias_scan_to_calibration.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     bias_scan_directory =  os.path.dirname(bias_scan_path)
 
     if args.vref is None:
-        if ( os.path.exists(f"{bias_scan_directory}/pedestal.root") or os.path.exists(f"{bias_scan_directory}/pedestals.root") ):
+        if os.path.exists(f"{bias_scan_directory}/pedestal.root") or os.path.exists(f"{bias_scan_directory}/pedestals.root"):
             vref = get_vref(bias_scan_directory)
         else:
             logging.warning("Could not find \"pedestal(s).root\" in the same folder. Using default value for reference voltage of 1.5V")
