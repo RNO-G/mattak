2dd5e99045df3eae655e274b9d76347f643f6fa2
Felix Schlueter <felix.schluter@icecube.wisc.edu>
2024-10-01 07:51:33 -0500

    Add comparison of EventInfo's returned by both backends

--- tests/compare_backends.py
+++ tests/compare_backends.py
@@ -4,6 +4,23 @@ import numpy
 import argparse
 
 
+def compare_event_info(ev1, ev2):
+
+    if not numpy.all(list(ev1.__dict__.keys()) == list(ev2.__dict__.keys())):
+        return False
+
+    for key in ev1.__dict__:
+        try:
+            if not ev1.__dict__[key] == ev2.__dict__[key]:
+                return False
+        except ValueError:
+            if not numpy.all(ev1.__dict__[key] == ev2.__dict__[key]):
+                print(key, ev1.__dict__[key].T, ev2.__dict__[key].T)
+                return False
+
+    return True
+
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Test mattak.")
     argparser.add_argument('--station', type=int, default=0)
@@ -17,20 +34,32 @@ if __name__ == "__main__":
 
     dset_pyroot = mattak.Dataset.Dataset(
         args.station, args.run, data_path=args.data_dir,
-        backend="pyroot", verbose=True, read_daq_status=False,
+        backend="pyroot", verbose=True,
         voltage_calibration=args.voltage_calibration)
 
     dset_uproot = mattak.Dataset.Dataset(
         args.station, args.run, data_path=args.data_dir,
-        backend="uproot", verbose=True, read_daq_status=False,
+        backend="uproot", verbose=True,
         voltage_calibration=args.voltage_calibration)
 
     dset_pyroot.setEntries((0, 100))
     dset_uproot.setEntries((0, 100))
 
     wfs = dset_pyroot.wfs(calibrated=calibrated)
+    print(wfs)
 
-    # 0.0001 -> 0.1 mV (1 ADC -> 0.61mV,  0.6 / sqrt(12) -> 1.76)
-    # at maximum of 700mV max tolerance is 0.1 mV + 0.001 * 700 mV = 0.17 mV
-    # at minmim of -1300mV max tolerance is 0.1mV + 0.001 * 1300 mV = 0.23 mV
-    assert numpy.allclose(wfs, dset_uproot.wfs(calibrated=calibrated), rtol=0.001, atol=0.0001), "Both backends return different waveforms."
+    if calibrated:
+        # 0.0001 -> 0.1 mV (1 ADC -> 0.61mV,  0.6 / sqrt(12) -> 1.76)
+        # at maximum of 700mV max tolerance is 0.1 mV + 0.001 * 700 mV = 0.17 mV
+        # at minmim of -1300mV max tolerance is 0.1mV + 0.001 * 1300 mV = 0.23 mV
+        assert numpy.allclose(wfs, dset_uproot.wfs(calibrated=calibrated), rtol=0.001, atol=0.0001), "Both backends return different waveforms."
+    else:
+        assert numpy.all(wfs == dset_uproot.wfs(calibrated=calibrated)), "Backends return different waveforms."
+
+
+    eventInfos_pyroot = dset_pyroot.eventInfo()
+    eventInfos_uproot = dset_uproot.eventInfo()
+    for ev_py, ev_up in zip(eventInfos_pyroot, eventInfos_uproot):
+        assert compare_event_info(ev_py, ev_up), "Backends return different event infos"
+
+    print("Successful")
