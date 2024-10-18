39b6aa9980bf7101a619a642f475c5dff7a8a3eb
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2024-10-18 11:18:09 -0500

    ws

--- src/mattak/Constants.h
+++ src/mattak/Constants.h
@@ -2,19 +2,19 @@
 #define __MATTAK_CONSTANTS_H__
 
 
-#include <stdint.h> 
+#include <stdint.h>
 
 
 namespace mattak 
 {
   namespace k
   {
-    constexpr uint8_t num_radiant_channels = 24; 
-    constexpr uint16_t num_radiant_samples = 2048; 
-    constexpr uint16_t num_lab4_samples = 4096; 
-    constexpr uint16_t num_lt_channels = 4; 
-    constexpr uint16_t radiant_window_size = 128; 
-    constexpr uint16_t radiant_windows_per_buffer = num_radiant_samples / radiant_window_size; 
+    constexpr uint8_t num_radiant_channels = 24;
+    constexpr uint16_t num_radiant_samples = 2048;
+    constexpr uint16_t num_lab4_samples = 4096;
+    constexpr uint16_t num_lt_channels = 4;
+    constexpr uint16_t radiant_window_size = 128;
+    constexpr uint16_t radiant_windows_per_buffer = num_radiant_samples / radiant_window_size;
   }
 }
 
