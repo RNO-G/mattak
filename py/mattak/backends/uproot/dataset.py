b8c50e27c5e1f36f70697c2f6e12a9a12ee08a4b
CamphynR <rubencamphyn@gmail.com>
2025-03-25 03:30:15 -0500

    add function to manually set calibration of dataset after init for testing purposes

--- py/mattak/backends/uproot/dataset.py
+++ py/mattak/backends/uproot/dataset.py
@@ -389,6 +389,17 @@ class Dataset(mattak.Dataset.AbstractDataset):
         """ Helper to access uproot file """
         return self._wfs[f'radiant_data[{self.NUM_CHANNELS}][{self.NUM_WF_SAMPLES}]'].array(**kw)
 
+    def set_calibration(self, voltage_calibration : str, cache_calibration : Optional[bool] = True):
+        """
+        Function to manually set a voltage calibration after the dataset is initialised
+        """
+        if hasattr(self, 'vc'):
+            del self.vc
+        if isinstance(voltage_calibration, str):
+            self.vc = VoltageCalibration(voltage_calibration, caching=cache_calibration)
+        else:
+            Raise(ValueError("This function only accepts path to voltage calibration"))
+
     def wfs(self, calibrated : bool = False, channels : Optional[Union[int, List[int]]] = None, override_skip_incomplete : Optional[bool] = None) -> Optional[numpy.ndarray]:
         """
         Returns the waveform data for the selected event(s).
