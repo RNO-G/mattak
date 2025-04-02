dfa01bd849a6e96c1ce8714e6246c9f99c6de19e
Ryan Krebs <ryankrebs016@gmail.com>
2025-04-02 15:09:29 -0400

    12 beams

--- src/mattak/Constants.h
+++ src/mattak/Constants.h
@@ -13,7 +13,7 @@ namespace mattak
     constexpr uint16_t num_radiant_samples = 2048; 
     constexpr uint16_t num_lab4_samples = 4096; 
     constexpr uint16_t num_lt_channels = 4; 
-    constexpr uint16_t num_lt_beams = 9; 
+    constexpr uint16_t num_lt_beams = 12;
     constexpr uint16_t radiant_window_size = 128; 
     constexpr uint16_t radiant_windows_per_buffer = num_radiant_samples / radiant_window_size; 
   }
