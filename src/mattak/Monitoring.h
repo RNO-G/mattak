43ebb7b41d83e061e505fb37ad066f2d5593779e
Felix Schlüter <30903175+fschlueter@users.noreply.github.com>
2026-03-02 09:44:37 +0100

    Clean up Monitoring.h by removing unused vectors
    Removed unused vectors for glitch counts and average block offset.

--- src/mattak/Monitoring.h
+++ src/mattak/Monitoring.h
@@ -22,7 +22,6 @@ namespace mattak
       std::vector<float> rms; // root mean square per channel
 
       std::vector<float> max_abs_amplitude;
-
       std::vector<float> glitching_test_statitic;
 
       std::vector<std::vector<float>> block_offset;
@@ -57,9 +56,6 @@ namespace mattak
       std::vector<std::vector<float>> avg_spectrum_lt;
       std::vector<std::vector<float>> avg_spectrum_rf0;
       std::vector<std::vector<float>> avg_spectrum_rf1;
-
-      std::vector<uint32_t> glitch_counts;
-      std::vector<std::vector<float>> avg_block_offset;
 
     ClassDef(RunSummary, 1);
   };
