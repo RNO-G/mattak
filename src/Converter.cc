fc51cde430817fe804ffb808217a62bd407e521d
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2026-07-16 17:49:52 +0200

    Remove trailing whitespaces

--- src/Converter.cc
+++ src/Converter.cc
@@ -1,8 +1,8 @@
-#include <sys/types.h> 
-#include <dirent.h> 
-#include <sys/stat.h> 
-#include <unistd.h> 
-#include <algorithm> 
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <algorithm>
 #include <vector>
 #include "TTree.h"
 #include "TFile.h"
@@ -16,13 +16,13 @@
 
 
 #ifdef LIBRNO_G_SUPPORT
-#include "rno-g.h" 
+#include "rno-g.h"
 
-template <typename T> const char * getName() { return "unnamed"; } 
-template<> const char * getName<mattak::Waveforms>() { return "waveforms"; } 
-template<> const char * getName<mattak::Header>() { return "header"; } 
-template<> const char * getName<mattak::DAQStatus>() { return "daqstatus"; } 
-template<> const char * getName<mattak::Pedestals>() { return "pedestals"; } 
+template <typename T> const char * getName() { return "unnamed"; }
+template<> const char * getName<mattak::Waveforms>() { return "waveforms"; }
+template<> const char * getName<mattak::Header>() { return "header"; }
+template<> const char * getName<mattak::DAQStatus>() { return "daqstatus"; }
+template<> const char * getName<mattak::Pedestals>() { return "pedestals"; }
 
 
 
@@ -30,47 +30,47 @@ template <typename Traw, int(*ReaderFn)(rno_g_file_handle_t, Traw*), typename Tr
 static int convert_impl(int N, const char ** infiles, const char * outfile, const char * treename, int station)
 
 {
-  TFile * f = 0; 
-  TTree *t = 0; 
-  Troot * b = 0; 
+  TFile * f = 0;
+  TTree *t = 0;
+  Troot * b = 0;
 
-  int nprocessed = 0; 
-  for (int i = 0; i < N; i++) 
+  int nprocessed = 0;
+  for (int i = 0; i < N; i++)
   {
 
-    rno_g_file_handle h; 
-    if (0 == rno_g_init_handle(&h, infiles[i], "r")) 
+    rno_g_file_handle h;
+    if (0 == rno_g_init_handle(&h, infiles[i], "r"))
     {
-      Traw raw; 
-      while (ReaderFn(h, &raw) > 0) 
+      Traw raw;
+      while (ReaderFn(h, &raw) > 0)
       {
-        if (!f) 
+        if (!f)
         {
-          f = new TFile(outfile,"RECREATE"); 
-          if (!treename) treename = getName<Troot>(); 
-          t = new TTree(treename,treename); 
-          b = new Troot; 
-          t->Branch(treename, &b); 
+          f = new TFile(outfile,"RECREATE");
+          if (!treename) treename = getName<Troot>();
+          t = new TTree(treename,treename);
+          b = new Troot;
+          t->Branch(treename, &b);
         }
 
-        nprocessed++; 
-        b = new (b) Troot(&raw); 
-        if (station > 0) b->station_number = station; 
-        t->Fill(); 
+        nprocessed++;
+        b = new (b) Troot(&raw);
+        if (station > 0) b->station_number = station;
+        t->Fill();
       }
-      rno_g_close_handle(&h); 
+      rno_g_close_handle(&h);
     }
 
   }
 
   if (f)
   {
-    f->Write(); 
+    f->Write();
     delete f;
-    f = 0; 
+    f = 0;
   }
 
-  return nprocessed; 
+  return nprocessed;
 }
 
 
@@ -79,119 +79,118 @@ static int convert_impl(int N, const char ** infiles, const char * outfile, cons
 template <typename Traw, int (*ReaderFn)(rno_g_file_handle_t, Traw*), typename Troot>
 static int convert_dir(const char * dir, const char * outfile, const char * treename, int station)
 {
-  std::vector<std::string> files; 
-  std::vector<const char *> file_ptrs; 
-  DIR * dirp = opendir(dir); 
+  std::vector<std::string> files;
+  std::vector<const char *> file_ptrs;
+  DIR * dirp = opendir(dir);
   if (!dirp)
   {
     ::Error("mattak::convert::convert_dir", "Could not open dir %s", dir);
     return 0;
   }
 
-  struct dirent * dent; 
+  struct dirent * dent;
 
 
   while ((dent = readdir(dirp)))
   {
-    std::string fname = dir; 
-    fname+="/"; 
-    fname+=dent->d_name; 
-    files.push_back(fname); 
+    std::string fname = dir;
+    fname+="/";
+    fname+=dent->d_name;
+    files.push_back(fname);
   }
 
-  closedir(dirp); 
-  std::sort(files.begin(), files.end()); 
+  closedir(dirp);
+  std::sort(files.begin(), files.end());
 
-  file_ptrs.reserve(files.size()); 
-  for (auto f : files) 
+  file_ptrs.reserve(files.size());
+  for (auto f : files)
   {
-    file_ptrs.push_back(f.c_str()); 
+    file_ptrs.push_back(f.c_str());
   }
 
-  return convert_impl<Traw,ReaderFn,Troot>(file_ptrs.size(), &file_ptrs[0], outfile, treename,station); 
+  return convert_impl<Traw,ReaderFn,Troot>(file_ptrs.size(), &file_ptrs[0], outfile, treename,station);
 }
 
 
-int mattak::convert::convertWaveformFiles(int nfiles, const char ** infiles, const char * outfile, const char * treename,int station) 
+int mattak::convert::convertWaveformFiles(int nfiles, const char ** infiles, const char * outfile, const char * treename,int station)
 {
-  return convert_impl<rno_g_waveform_t, rno_g_waveform_read, mattak::Waveforms>(nfiles, infiles, outfile, treename,station); 
+  return convert_impl<rno_g_waveform_t, rno_g_waveform_read, mattak::Waveforms>(nfiles, infiles, outfile, treename,station);
 }
 
 int mattak::convert::convertWaveformFile(const char * infile, const char * outfile, const char * treename,int station)
 {
-  return convert_impl<rno_g_waveform_t, rno_g_waveform_read, mattak::Waveforms>(1, &infile, outfile, treename,station); 
+  return convert_impl<rno_g_waveform_t, rno_g_waveform_read, mattak::Waveforms>(1, &infile, outfile, treename,station);
 }
 
-int mattak::convert::convertWaveformDir(const char * dir, const char * outfile, const char * treename,int station) 
+int mattak::convert::convertWaveformDir(const char * dir, const char * outfile, const char * treename,int station)
 {
-  return convert_dir<rno_g_waveform_t, rno_g_waveform_read, mattak::Waveforms>(dir, outfile, treename,station); 
+  return convert_dir<rno_g_waveform_t, rno_g_waveform_read, mattak::Waveforms>(dir, outfile, treename,station);
 }
 
-int mattak::convert::convertHeaderFiles(int nfiles, const char ** infiles, const char * outfile, const char * treename,int station) 
+int mattak::convert::convertHeaderFiles(int nfiles, const char ** infiles, const char * outfile, const char * treename,int station)
 {
-  return convert_impl<rno_g_header_t, rno_g_header_read, mattak::Header>(nfiles, infiles, outfile, treename,station); 
+  return convert_impl<rno_g_header_t, rno_g_header_read, mattak::Header>(nfiles, infiles, outfile, treename,station);
 }
 
 int mattak::convert::convertHeaderFile(const char * infile, const char * outfile, const char * treename,int station)
 {
-  return convert_impl<rno_g_header_t, rno_g_header_read, mattak::Header>(1, &infile, outfile, treename,station); 
+  return convert_impl<rno_g_header_t, rno_g_header_read, mattak::Header>(1, &infile, outfile, treename,station);
 }
 
-int mattak::convert::convertHeaderDir(const char * dir, const char * outfile, const char * treename,int station) 
+int mattak::convert::convertHeaderDir(const char * dir, const char * outfile, const char * treename,int station)
 {
-  return convert_dir<rno_g_header_t, rno_g_header_read, mattak::Header>(dir, outfile, treename,station); 
+  return convert_dir<rno_g_header_t, rno_g_header_read, mattak::Header>(dir, outfile, treename,station);
 }
 
-int mattak::convert::convertDAQStatusFiles(int nfiles, const char ** infiles, const char * outfile, const char * treename,int station) 
+int mattak::convert::convertDAQStatusFiles(int nfiles, const char ** infiles, const char * outfile, const char * treename,int station)
 {
-  return convert_impl<rno_g_daqstatus_t, rno_g_daqstatus_read, mattak::DAQStatus>(nfiles, infiles, outfile, treename,station); 
+  return convert_impl<rno_g_daqstatus_t, rno_g_daqstatus_read, mattak::DAQStatus>(nfiles, infiles, outfile, treename,station);
 }
 
 int mattak::convert::convertDAQStatusFile(const char * infile, const char * outfile, const char * treename,int station)
 {
-  return convert_impl<rno_g_daqstatus_t, rno_g_daqstatus_read, mattak::DAQStatus>(1, &infile, outfile, treename,station); 
+  return convert_impl<rno_g_daqstatus_t, rno_g_daqstatus_read, mattak::DAQStatus>(1, &infile, outfile, treename,station);
 }
 
-int mattak::convert::convertDAQStatusDir(const char * dir, const char * outfile, const char * treename,int station) 
+int mattak::convert::convertDAQStatusDir(const char * dir, const char * outfile, const char * treename,int station)
 {
-  return convert_dir<rno_g_daqstatus_t, rno_g_daqstatus_read, mattak::DAQStatus>(dir, outfile, treename,station); 
+  return convert_dir<rno_g_daqstatus_t, rno_g_daqstatus_read, mattak::DAQStatus>(dir, outfile, treename,station);
 }
 
-int mattak::convert::convertPedestalFiles(int nfiles, const char ** infiles, const char * outfile, const char * treename,int station) 
+int mattak::convert::convertPedestalFiles(int nfiles, const char ** infiles, const char * outfile, const char * treename,int station)
 {
-  return convert_impl<rno_g_pedestal_t, rno_g_pedestal_read, mattak::Pedestals>(nfiles, infiles, outfile, treename,station); 
+  return convert_impl<rno_g_pedestal_t, rno_g_pedestal_read, mattak::Pedestals>(nfiles, infiles, outfile, treename,station);
 }
 
 int mattak::convert::convertPedestalFile(const char * infile, const char * outfile, const char * treename,int station)
 {
-  return convert_impl<rno_g_pedestal_t, rno_g_pedestal_read, mattak::Pedestals>(1, &infile, outfile, treename,station); 
+  return convert_impl<rno_g_pedestal_t, rno_g_pedestal_read, mattak::Pedestals>(1, &infile, outfile, treename,station);
 }
 
-int mattak::convert::convertPedestalDir(const char * dir, const char * outfile, const char * treename,int station) 
+int mattak::convert::convertPedestalDir(const char * dir, const char * outfile, const char * treename,int station)
 {
-  return convert_dir<rno_g_pedestal_t, rno_g_pedestal_read, mattak::Pedestals>(dir, outfile, treename,station); 
+  return convert_dir<rno_g_pedestal_t, rno_g_pedestal_read, mattak::Pedestals>(dir, outfile, treename,station);
 }
 
 
 #endif
 
-int mattak::convert::makeRunInfo(const char *auxdir, const char * outfile, int station_override, int run_override) 
+int mattak::convert::makeRunInfo(const char *auxdir, const char * outfile, int station_override, int run_override)
 {
-  TFile of(outfile,"RECREATE"); 
-  RunInfo * ri = new RunInfo(auxdir); 
-  if (station_override > 0) 
+  TFile of(outfile,"RECREATE");
+  RunInfo * ri = new RunInfo(auxdir);
+  if (station_override > 0)
   {
-    ri->station = station_override; 
+    ri->station = station_override;
   }
 
-  if (run_override > 0) 
+  if (run_override > 0)
   {
-    ri->run = run_override; 
+    ri->run = run_override;
   }
 
-//  ri->Dump(); 
-  ri->Write("info"); 
+//  ri->Dump();
+  ri->Write("info");
   of.Close();
-  return 0; 
+  return 0;
 }
-
