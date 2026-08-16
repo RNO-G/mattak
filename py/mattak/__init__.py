141c2e81f18b1397e256f86d8cd890c9566f5d27
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2026-08-16 18:13:04 +0200

    Fix

--- py/mattak/__init__.py
+++ py/mattak/__init__.py
@@ -16,8 +16,6 @@ def _setup_logger(name="mattak"):
     ----------
     name : str, default="mattak"
         The name of the base logger
-    level : int, default=25
-        The logging level to use for the base logger
 
     """
     logger = logging.getLogger(name)
@@ -59,18 +57,23 @@ def get_fancy_formatter():
             red = "\033[31;1m"
             reset = "\033[0m"
 
+            # One formatter per level, built once: constructing them in format() would
+            # drop `datefmt` and allocate a Formatter per record.
             self.FORMATS = {
-                logging.DEBUG: grey + "%(levelname)s - " + reset + format,
-                logging.INFO: green + "%(levelname)s - " + reset + format,
-                logging.WARNING: purple + "%(levelname)s - " + reset + format,
-                logging.ERROR: red + "%(levelname)s - " + reset + format,
-                logging.CRITICAL: red + "%(levelname)s - " + reset + format
+                level: logging.Formatter(color + "%(levelname)s - " + reset + format, datefmt=datefmt)
+                for level, color in [
+                    (logging.DEBUG, grey),
+                    (logging.INFO, green),
+                    (logging.WARNING, purple),
+                    (logging.ERROR, red),
+                    (logging.CRITICAL, red),
+                ]
             }
+            # Used for custom levels (e.g. 25), which have no color assigned
+            self.default_format = logging.Formatter(format, datefmt=datefmt)
 
         def format(self, record):
-            log_fmt = self.FORMATS.get(record.levelno)
-            formatter = logging.Formatter(log_fmt)
-            return formatter.format(record)
+            return self.FORMATS.get(record.levelno, self.default_format).format(record)
 
 
     formatter = CustomFormatter(
